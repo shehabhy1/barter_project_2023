bc1c41366802cee91c500eef9ef65aa1b0850b13
Shehab <95573109+shehabhy1@users.noreply.github.com>
2023-08-02 14:33:39 +0300

    replacing wrong names

--- lib/features/splash_view/presentation/views/on_boarding_view.dart
+++ lib/features/splash_view/presentation/views/on_boarding_view.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:barter_project_2023/constants.dart';
-import 'package:barter_project_2023/features/splash_view/presentation/views/widgets/indicatour.dart';
+import 'package:barter_project_2023/features/splash_view/presentation/views/widgets/indicator.dart';
 import 'package:barter_project_2023/features/splash_view/presentation/views/widgets/on_boarding_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';

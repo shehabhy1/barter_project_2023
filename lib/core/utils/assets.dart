263c59a325632d9dfb797c9438bf7c66d90aaebc
Mostafa Esmail <mostafaesmail9244@gmail.com>
2023-08-04 16:27:12 +0300

    add nav bar icons

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -3,9 +3,9 @@ class AssetData {
   static const String logo = 'assets/images/logo.png';
   static const String onboardingImage1 = 'assets/images/on_boarding_Image1.png';
   static const String onboardingImage2 = 'assets/images/on_boarding_Image2.png';
-  static String homeIcon = 'assets/images/home2.png';
-  static String notificationIcon = 'assets/images/notification.png';
-  static String addIcon = 'assets/images/addsquare.png';
-  static String profilIcon = 'assets/images/profilecircle.png';
-  static const String notiPhoto ='assets/images/notiPhoto.jpg';
+  static String homeIcon = 'assets/images/home.svg';
+  static String notificationIcon = 'assets/images/notification.svg';
+  static String addIcon = 'assets/images/addsquare.svg';
+  static String profilIcon = 'assets/images/profilecircle.svg';
+  static const String notiPhoto = 'assets/images/notiPhoto.jpg';
 }

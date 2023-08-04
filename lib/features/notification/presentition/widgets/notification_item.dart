71903c0753dd54fd2fbca4535407eac8bcaf2237
Mostafa Esmail <mostafaesmail9244@gmail.com>
2023-08-04 12:32:24 +0300

    edite notification item padding

--- lib/features/notification/presentition/widgets/notification_item.dart
+++ lib/features/notification/presentition/widgets/notification_item.dart
@@ -2,16 +2,18 @@ import 'package:barter_project_2023/features/notification/presentition/widgets/n
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
-import '../../../../core/utils/assets.dart';
-
 class NotificationItem extends StatelessWidget {
   const NotificationItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //TODO: replace fixed size to adaptive size => sizer pack
+    //TODO: replace fixed size to adaptive size => sizer package
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 16),
+      padding: EdgeInsets.only(
+        right: 5.w,
+        left: 5.w,
+        bottom: 16,
+      ),
       child: Container(
         decoration: BoxDecoration(
           color: Colors.black12,

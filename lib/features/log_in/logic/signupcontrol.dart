88fc61f70fc55b726c377e4b92843d4528986ced
Shehab <shehabhy1@gmail.com>
2024-05-03 23:26:11 +0300

    upgrading versions

--- lib/features/log_in/logic/signupcontrol.dart
+++ lib/features/log_in/logic/signupcontrol.dart
@@ -9,8 +9,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as p;
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ControlSignUp extends GetxController {
+  
   final auth = FirebaseAuth.instance;
   bool isLoading = true;
   File? file;

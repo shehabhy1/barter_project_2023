6bd56d2bf17bfb98cf06b937fe8f70b9a13aed58
Mostafa_Esmail <mostafaesmail9244@gmail.com>
2024-05-04 18:16:24 +0300

    - undo last merge

--- lib/features/splash_view/presentation/views/splash_view.dart
+++ lib/features/splash_view/presentation/views/splash_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../../core/utils/app_router.dart';
 import '../../../../core/utils/assets.dart';
